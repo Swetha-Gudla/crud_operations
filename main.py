0ee8de82abd276315915700ef43481f5b7901e3d
Swetha-Gudla <swethagudla.500apps@gmail.com>
2023-08-22 15:32:13 +0530

    implemented crud operations

---
+++ main.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+app=APIRouter()
+
+db={}
+
+class Items(BaseModel):
+    name: str
+    desc : str
+
+# @app.get("/")
+# async def root(name: str):
+#     return{"message":"Welcome "+name}
+
+@app.get("/")
+def get_all_data():
+    return db
+
+@app.post("/items")
+def Create_Item(item:Items):
+    db[item.name]=item.desc
+    return {"items":item}
+
+@app.get("/")
+def get_all_data():
+    return db
+
+@app.delete("/")
+def delete_data(name:str):
+    del db[name]
+    return db
+
+@app.put("/")
+def update_data(item:Items):
+    db[item.name]=item.desc
+    return db
+
+
+if __name__ == '__main__':
+    app.run()
+    
+
